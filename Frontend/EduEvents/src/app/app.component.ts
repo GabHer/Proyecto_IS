90ce672c06b1695c8c63467410de8fa910ca074d
GabHer <Gehc199929>
2021-11-02 23:01:57 -0600

    Agregada Pantalla Eventos.

--- Frontend/EduEvents/src/app/app.component.ts
+++ Frontend/EduEvents/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgwWowService } from 'ngx-wow';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EduEvents';
+  constructor( private wowService: NgwWowService ) {
+  this.wowService.init();
+  }
 }
