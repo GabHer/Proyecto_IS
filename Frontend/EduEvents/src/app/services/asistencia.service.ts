06130cc8eae56158cac551bbbaa9bfe32ecc7de5
GabHer <Gehc199929>
2021-12-06 12:20:45 -0600

    Guardar Asistencias

---
+++ Frontend/EduEvents/src/app/services/asistencia.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AsistenciaService{
+  constructor( private httpClient: HttpClient ) { }
+
+  enviarAsistencias(objAsistencia:any){
+    return this.httpClient.post(`http://localhost:8888/asistencia`, objAsistencia);
+  }
+}
