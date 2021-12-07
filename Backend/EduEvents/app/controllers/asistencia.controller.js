45fd471b11ecc191408c97dd7b9331a0f0aadaa4
unknown <xeniaalfaro99@hotmail.com>
2021-12-06 22:47:43 -0600

    Cambios en BD;Se modifica método para guardar Asistencias;Obtener campo Emision_Asistencias de una conferencia.

--- Backend/EduEvents/app/controllers/asistencia.controller.js
+++ Backend/EduEvents/app/controllers/asistencia.controller.js
@@ -2,7 +2,6 @@ const Asistencia = require("../models/asistencia.model");
 
 // Actualizar el campo asistencia para todas las personas incritas a una conferencia.
 exports.actualizarAsistencia = (req, res) => {
-<<<<<<< HEAD
     if(!req.body) {
       res.status(400).send({
         message: "El contenido no puede ser vacío"
@@ -12,22 +11,22 @@ exports.actualizarAsistencia = (req, res) => {
   
     Asistencia.actualizar(req.body, (err, data) => {
     
-      if(err) {
+    if(err) {
         console.log(err);
         res.status(500).send({
         mensaje: "Error al actualizar el campo de 'Asistencia' para los inscritos en la conferencia con Id: " + req.body.idConferencia
       });
     } 
     else {
-      res.send({
-        mensaje: `Se actualizó el campo de 'Asistencia' para los inscritos en la conferencia con Id: ${req.body.idConferencia}.`, codigo:200, estado:'ok', data: data
+      res.status(200).send({
+        mensaje: `Se actualizó el campo de 'Asistencia' para los inscritos en la conferencia con Id: ${req.body.idConferencia}.`, codigo:200, estado:'ok'
       });
     };
   });
 };
 
 // Obtener datos para renderizar diplomas
-exports.obtenerAsistenciaConferencia = (req, res) => {
+exports.obtenerDatosDiploma = (req, res) => {
   if(!req.params) {
     res.status(400).send({
       message: "El contenido no puede ser vacio"
@@ -61,33 +60,35 @@ exports.obtenerAsistenciaConferencia = (req, res) => {
 
 
 
-exports.emicion = (req, res) => {
-if(!req.params) {
-    res.status(400).send({
-    message: "El contenido no puede ser vacío"
-    });
-    return;
-};
+exports.asistenciaEmision = (req, res) => {
+    if(!req.params) {
+        res.status(400).send({
+        message: "El contenido no puede ser vacío"
+        });
+        console.log("entra1")
+        return;
+    };
 
-    Asistencia.emicion(req.params.idConferencia, (err, data) => {
+    Asistencia.emision(req.params, (err, data) => {
 
         if(err) {
-        console.log(err);
-        res.status(500).send({
-        mensaje: "Error al obtener si se ha emitido la asitencia o no, para la conferencia con id : " + req.body.idConferencia
-        });
+            console.log(err);
+            res.status(500).send({
+            mensaje: "Error al obtener si se ha emitido la asitencia o no, para la conferencia con id : " + req.params.idConferencia
+            });
         } 
 
-        if (data.estado === "no_encontrado") {
-        res.status(404).send({
-            mensaje: `El id de Conferencia no exite.`, codigo:404, data:null
-        });
-        }
+        if (data.estado == "no_encontrado") {
+            res.status(404).send({
+                mensaje: `El id de Conferencia no exite.`, codigo:404, data:null
+            });
+         }
 
         else {
-        res.status(200).send({
-            mensaje: `Se ha obtenido si se ha emitido la lista de asistencia o no para la conferencia con id: ${req.body.idConferencia}.`, codigo:200, estado:'ok', data: data
-        });
+
+            res.status(200).send({
+                mensaje: `Se ha obtenido si se ha emitido la lista de asistencia o no para la conferencia con id: ${req.params.idConferencia}.`, codigo:200, estado:'ok', data: data
+            });
         };
     });
 };
--- Backend/EduEvents/app/models/asistencia.model.js
+++ Backend/EduEvents/app/models/asistencia.model.js
@@ -9,6 +9,8 @@ const Asistencia = function(objAsistencia) {
 Asistencia.actualizar = (objetoAsistencia, resultado) => {
 
     //Consulta que actualizara el campo de Asitencia a 1 para aquellos usuarios que si asistieron a la conferencia.
+
+    var filasAfectadas = 0;
     for (let i=0; i<objetoAsistencia.listaAsistencia.length; i++) {
         var consultaAsistencia = `UPDATE Persona_Conferencia SET Asistencia = 1 WHERE Id_Persona = ${objetoAsistencia.listaAsistencia[i]} AND Id_Conferencia = ${objetoAsistencia.idConferencia}`;
 
@@ -17,37 +19,58 @@ Asistencia.actualizar = (objetoAsistencia, resultado) => {
               // Si ocurre un error con la consulta
               resultado(err, null);
               return;
-            };
-
-            //Solo si se actualizaron filas.
-            if(res.affectedRows!=0){
-                //Consulta que actualizara el campo de Asitencia a 0 para aquellos usuarios que no asistieron a la conferencia.
-                var consultaNoAsistencia = `UPDATE Persona_Conferencia SET Asistencia = 0 WHERE Asistencia IS NULL AND Id_Conferencia = ${objetoAsistencia.idConferencia}`;
-
-                sql.query(consultaNoAsistencia, (err, res) => {
-                    if(err){
-                    // Si ocurre un error con la consulta
-                    resultado(err, null);
-                    return;
-                    };
-
-                    resultado(null, res)
-                });
-            }
-
-            else{
-                resultado(null, {estado: "no_encontrado"})
-            };
+            } 
         });
     };
+
+    //Consulta que actualizara el campo de Asitencia a 0 para aquellos usuarios que no asistieron a la conferencia.
+    var consultaNoAsistencia = `UPDATE Persona_Conferencia SET Asistencia = 0 WHERE Asistencia IS NULL AND Id_Conferencia = ${objetoAsistencia.idConferencia}`;
+
+    sql.query(consultaNoAsistencia, (err, res) => {
+        if(err){
+        // Si ocurre un error con la consulta
+        resultado(err, null);
+        return;
+        
+        };
+
+        //Actualizando Asistencia = 1 para El encargado
+        var consultaNoAsistencia = `UPDATE Persona_Conferencia SET Asistencia = 1 WHERE Id_Persona = (SELECT DISTINCT Persona.Id From Persona JOIN Conferencia ON Persona.Correo = Conferencia.Correo_Encargado WHERE Conferencia.Id = ${objetoAsistencia.idConferencia}) AND Id_Conferencia = ${objetoAsistencia.idConferencia};
+        `;
+
+        sql.query(consultaNoAsistencia, (err, res) => {
+            if(err){
+            // Si ocurre un error con la consulta
+            resultado(err, null);
+            return;
+            };
+
+            var consultaNoAsistencia = `UPDATE Conferencia SET Emision_Asistencia = 1 WHERE Id = ${objetoAsistencia.idConferencia};
+            `;
+
+            sql.query(consultaNoAsistencia, (err, res) => {
+                if(err){
+                // Si ocurre un error con la consulta
+                resultado(err, null);
+                return;
+                }
+                
+                else {
+                    resultado(null, res);
+                    return;
+                };
+            });
+        });
+    });
+
 };
+  
 
-
-Asistencia.emicion = (objetoAsistencia, resultado) => {
+Asistencia.emision = (objetoAsistencia, resultado) => {
 
     //Consulta que obtedrá si la lista de asistencia ya fué emitida
     
-    var consultaEmicion = `SELECT Emision_Asistencia FROM Conferencia WHERE Id = ${objetoAsistencia.IdConferencia}`;
+    var consultaEmicion = `SELECT Emision_Asistencia FROM Conferencia WHERE Id = ${objetoAsistencia.idConferencia}`;
 
     sql.query(consultaEmicion, (err, res) => {
         if(err){
@@ -56,13 +79,14 @@ Asistencia.emicion = (objetoAsistencia, resultado) => {
             return;
         }
 
-        if(res.fieldCount!=0){
-            resultado(null, res) 
+        if(res.length){
+            resultado(null, res);
+            return;
         }
 
-
         else {
-            resultado(null, {estado: "no_encontrado"})
+            resultado(null, {estado: "no_encontrado"});
+            return;
         }; 
     });
 };
