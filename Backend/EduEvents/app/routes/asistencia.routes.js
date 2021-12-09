47fb0722421441610200bf5b6bd4f0125e243b6d
unknown <xeniaalfaro99@hotmail.com>
2021-12-08 18:45:46 -0600

    Se hacen cambios para veririficar funcionamiento.

--- Backend/EduEvents/app/routes/asistencia.routes.js
+++ Backend/EduEvents/app/routes/asistencia.routes.js
@@ -4,16 +4,9 @@ module.exports = app => {
   // Actualizar campo Asistencia para los asistentes y no asistentes.
   app.post("/asistencia", asistencia.actualizarAsistencia);
 
-<<<<<<< HEAD
-    app.get('/asistencia/asistenciaEmision/:idConferencia', asistencia.asistenciaEmision);
-=======
-  // Obtener los datos del diploma
-  app.get('/asistencia/obtenerDatosDiploma/:idConferencia/:idPersona', asistencia.obtenerDatosDiploma);
-
   app.get('/asistencia/asistenciaEmision/:idConferencia', asistencia.asistenciaEmision);
 
   // Obtener lista de asistencia para una conferencia
   app.get('/asistencia/obtenerListaAsistenciaPorIdConferencia/:idConferencia', asistencia.obtenerListaAsistencia)
->>>>>>> 1e26a3b57bf59960532947c5676a540867d72ffd
   
 };
