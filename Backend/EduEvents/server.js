16c1b03c12da85f4aa5ea6826d7e8674c9c71f68
Amilcar Antonio Rodriguez Velasquez <aarodriguezv@unah.hn>
2022-05-02 20:14:30 -0600

    CORS

--- Backend/EduEvents/server.js
+++ Backend/EduEvents/server.js
@@ -3,7 +3,28 @@ const dotenv = require("dotenv")
 const app = express();
 // Acceso mediante cors para todos los clientes
 const cors = require('cors');
+dotenv.config()
+
 app.use(express.json({limit: "500mb"}))
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENTE || 'https://edu-events.herokuapp.com');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use(cors({
   origin: '*',
@@ -12,7 +33,7 @@ app.use(cors({
 }))
 
 app.use(express.json())
-dotenv.config()
+
 
 app.set('port', process.env.PORT || 4000)
 
