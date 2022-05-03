5e1a13cc0e7a2acf6d613c6fc292cc40c4b11071
Amilcar Antonio Rodriguez Velasquez <aarodriguezv@unah.hn>
2022-05-02 21:30:49 -0600

    CORS

--- Backend/EduEvents/server.js
+++ Backend/EduEvents/server.js
@@ -1,65 +1,40 @@
 const express = require("express");
 const dotenv = require("dotenv")
-const app = express();
-// Acceso mediante cors para todos los clientes
 const cors = require('cors');
-dotenv.config()
 
+const personaRoutes = require("./app/routes/persona.routes.js");
+const loginRoutes = require("./app/routes/login.routes.js");
+const eventoRoutes = require("./app/routes/evento.routes.js");
+const conferenciaRoutes = require("./app/routes/conferencia.routes.js");
+const inscripcionRoutes = require("./app/routes/inscripcion.routes.js");
+const asistenciaRoutes = require("./app/routes/asistencia.routes.js");
+const diplomaRoutes = require("./app/routes/diploma.routes.js");
+
+const app = express();
+app.use(cors())
 app.use(express.json({limit: "500mb"}))
 
-app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENTE || 'https://edu-events.herokuapp.com');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
-app.use(cors({
-  origin: '*',
-  credentials: true,
-  optionsSuccessStatus: 200
-}))
 
 app.use(express.json())
+dotenv.config()
 
+// Routing 
 
-app.set('port', process.env.PORT || 4000)
-
-
-// Hacemos un parse a json y definimos un limite
-//app.use(express.json({limit: '50mb', extended: true,parameterLimit:50000}));
-
-
-app.use(express.urlencoded({limit:"500mb", extended: true, parameterLimit:500000}));
-
-// parse requests of content-type: application/x-www-form-urlencoded
-app.use(express.urlencoded({extended: true})); 
-
-// Ruta raiz
 app.get("/", (req, res) => {
   res.json({ message: "Bienvenido a EduEvents" });
 });
 
-require("./app/routes/persona.routes.js")(app);
-require("./app/routes/login.routes.js")(app);
-require("./app/routes/evento.routes.js")(app);
-require("./app/routes/conferencia.routes.js")(app);
-require("./app/routes/inscripcion.routes.js")(app);
-require("./app/routes/asistencia.routes.js")(app);
-require("./app/routes/diploma.routes.js")(app);
 
+
+
+personaRoutes(app)
+loginRoutes(app)
+eventoRoutes(app)
+conferenciaRoutes(app)
+inscripcionRoutes(app)
+asistenciaRoutes(app)
+diplomaRoutes(app)
 
 const PORT = process.env.PORT || 4000
 
